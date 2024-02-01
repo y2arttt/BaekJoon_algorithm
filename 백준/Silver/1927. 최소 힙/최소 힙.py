7bde3ba4e67457e35d80d1b33060cac8c1709968
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-02-01 18:14:02 +0900

    [Silver II] Title: 최소 힙, Time: 372 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,47 @@
+import sys
+
+heapq = []
+result = []
+
+
+def insert(i):
+    if i > 0 and heapq[(i-1)//2] > heapq[i]:
+        heapq[(i-1)//2],heapq[i] = heapq[i],heapq[(i-1)//2]
+        insert((i-1)//2)
+    return
+
+def delete(i):
+    if i*2+2 < len(heapq) and i*2+1 < len(heapq) and heapq[i] > heapq[i*2+2] and heapq[i] > heapq[i*2+1] :
+        if heapq[i*2+2] > heapq[i*2+1]:
+            heapq[i*2+1],heapq[i] = heapq[i],heapq[i*2+1]
+            delete(i*2+1)
+        else:
+            heapq[i*2+2],heapq[i] = heapq[i],heapq[i*2+2]
+            delete(i*2+2)
+    elif i*2+2 < len(heapq) and heapq[i] > heapq[i*2+2]:
+        heapq[i*2+2],heapq[i] = heapq[i],heapq[i*2+2]   
+        delete(i*2+2) 
+    elif  i*2+1 < len(heapq) and heapq[i] > heapq[i*2+1]:
+        heapq[i*2+1],heapq[i] = heapq[i],heapq[i*2+1]
+        delete(i*2+1)
+    return
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    t = int(sys.stdin.readline())
+    if t == 0:
+        if heapq:
+            if len(heapq) > 1:
+                result.append(heapq[0])
+                heapq[0] = heapq.pop(len(heapq)-1)
+                delete(0)
+            else:
+                result.append(heapq.pop(0))
+        else:
+            result.append(0)
+    else:    
+        heapq.append(t)
+        insert(len(heapq)-1)
+for i in result:
+    print(i)
+    
