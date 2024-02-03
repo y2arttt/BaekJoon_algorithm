34f922002aa022e04813ffc35611b0821feb620f
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-02-04 00:59:16 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 108 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -7,12 +7,12 @@ stack = deque()
 while True:   
     st = sys.stdin.readline().rstrip()
     stack.clear()
-    if st[0] == '.':
+    if st[0] == '.': 
         break
     else:
-        for i in st:
+        for i in st: #핵심: 괄호를 닫을때 여는 괄호와 쌍으로 지움
             if i == '(' or i == '[':
-                stack.append(i)
+                stack.append(i) #스택에 여는 문장을 추가
             elif i == ')' or i == ']':
                 if len(stack) > 0:
                     t1 = stack.pop()
@@ -20,10 +20,11 @@ while True:
                         continue
                     elif t1 == '[' and i == ']':
                         continue
-                stack.append(-1)          
-            elif i == '.' :
+                stack.append(-1) # 괄호가 제대로 끝마쳐지지 않으면 스택을 비우지 않음
+                break
+            elif i == '.' : 
                break
-        if stack:
+        if stack: #스택이 있음: 마지막이 끝맺어지지 않음 or -1
             result.append("no")   
         else:
             result.append("yes")
