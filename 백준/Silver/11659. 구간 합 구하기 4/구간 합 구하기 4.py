eaafb0e8c86f25adbb788ceb5379d9f0de75f3cc
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-02-06 00:04:01 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 240 ms, Memory: 41116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -4,10 +4,10 @@ result = []
 n,m = map(int,sys.stdin.readline().split())
 number = [0] + list(map(int,sys.stdin.readline().split()))
 for i in range(2,n+1):
-    number[i] += number[i-1] 
+    number[i] += number[i-1] # 순서대로 누적 합 구하기
     
 for _ in range(m):
     i,j = map(int,sys.stdin.readline().split())
-    result.append(number[j]-number[i-1])
+    result.append(number[j]-number[i-1]) # 구간만 남기고 나머지 값을 누적 값에서 빼버림
 for i in result:
     print(i)
