8847676503f92751fc0d0398c236e0272ed8b6c7
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-07-25 01:22:25 +0900

    [Gold IV] Title: 플로이드, Time: 28 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#define INF INT_MAX
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int N, M;
+    cin>>N>>M;
+    int map[N+1][N+1];
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            if(i==j) map[i][j] = 0;
+            else map[i][j] = INF;
+        }
+    }
+    for(int i=0; i<M; i++){
+        int s,e,w;
+        cin>>s>>e>>w;
+        map[s][e] = min(map[s][e], w);
+    }
+    for(int k=1; k<=N; k++){
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                if(map[i][k] != INF && map[k][j] != INF)
+                    map[i][j] = min(map[i][j],map[i][k] + map[k][j]);
+            }
+        }
+    }
+    for(int i=1; i<=N; i++){
+        for(int j=1; j<=N; j++){
+            if(map[i][j] == INF) cout << "0 ";
+            else cout<<map[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
+    return 0;
+}
