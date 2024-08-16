e185f59acccddf6133485e3680b375f6e5128b9c
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-08-17 03:07:58 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+
+int p[201];
+
+int find(int x){
+	if(p[x] == x) return x;
+	return p[x] = find(p[x]);
+}
+
+void uni(int x, int y){
+	x = find(x);
+	y = find(y);
+	if(x < y) p[y] = x;
+	else p[x] = y;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	int n,m,cnt=0;
+	cin >> n >> m;
+	
+	for(int i=1; i<=n; i++) p[i] = i;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1; j<=n; j++){
+			int x;
+			cin >> x;
+			if(x == 1) {
+				uni(i,j);
+			}
+		}
+	}
+	int root;
+	for(int i=0; i<m; i++){
+		int x;
+		cin >> x;
+		if(i == 0) root = find(x);
+		else{
+			if(find(root) != find(x)){
+				cout << "NO";
+				return 0;
+			}
+		}
+	}
+	cout << "YES";
+	return 0;
+}
