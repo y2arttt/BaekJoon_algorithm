285640afa5c17f39ffa960e8aacad33579d26925
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-08-17 15:16:40 +0900

    [Gold III] Title: 치즈, Time: 12 ms, Memory: 2300 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.cc
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -7,14 +7,17 @@ using namespace std;
 int map[100][100];
 int air[100][100];
 int n, m;
-queue<pair<int, int>> idx;
+queue<pair<int, int>> idx;//
 bool visited[100][100];
 int moves[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
 void airCheck() {
     queue<pair<int, int>> q;
-    q.push({0, 0});
+	memset(air, 0, sizeof(air));
+	memset(visited, false, sizeof(visited));
+	q.push({0, 0});
     visited[0][0] = true; 
+
     while (!q.empty()) {
         int i = q.front().first;
         int j = q.front().second;
@@ -35,24 +38,7 @@ void airCheck() {
     }
 }
 
-bool cheese() {
-    bool melted = false;
-    int size = idx.size();
-    for (int k = 0; k < size; k++) {
-        int i = idx.front().first;
-        int j = idx.front().second;
-        idx.pop();
 
-        if (air[i][j] >= 2) {
-            map[i][j] = 0;
-            melted = true;
-        } else {
-            idx.push({i, j});
-        }
-    }
-
-    return melted;
-}
 
 int main(void) {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -61,23 +47,25 @@ int main(void) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) { 
             cin >> map[i][j];
-            if (map[i][j] == 1) {
-                idx.push({i, j});
-            }
+
         }
     }
-
-    while (!idx.empty()) {
-        memset(air, 0, sizeof(air));
-        memset(visited, false, sizeof(visited));
-
+    while (true) {
+		bool change = false;
         airCheck();
-
-        if (!cheese()) break; 
+		for (int i=0; i<n; i++) {
+			for(int j=0; j<m; j++){
+				if (air[i][j] >= 2) {
+					map[i][j] = 0;
+					change= true;
+				}
+			}
+		}
         cnt++; 
+		if(!change)break;
     }
 
-    cout << cnt;
+    cout << cnt-1;
 
     return 0;
 }
