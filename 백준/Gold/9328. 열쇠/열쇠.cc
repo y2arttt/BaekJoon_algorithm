2723d4903ee911cf27f348927241ff02b3ce6dfb
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-08-31 22:33:28 +0900

    [Gold I] Title: 열쇠, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.cc
+++ 백준/Gold/9328. 열쇠/열쇠.cc
@@ -2,12 +2,11 @@
 #include <vector>
 #include <queue>
 #include <string>
+#include <cstring>  
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
     int t;
     cin >> t;
@@ -16,27 +15,45 @@ int main() {
         cin >> n >> m;
 
         vector<pair<int, int>> start;
-        char map[100][100];
-
+        char map[102][102];  
+        bool check[102][102]; 
+        memset(check, false, sizeof(check)); 
+        memset(map, '.', sizeof(map));  
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 cin >> map[i][j];
-                if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
-                    if (map[i][j] != '*') start.push_back({i, j});
-                }
             }
         }
+        
+        // 덧댄 면
+        for (int i = 0; i <= n+1; i++) {
+            start.push_back({i, 0});
+            start.push_back({i, m+1});
+        }
+        for (int j = 1; j <= m; j++) {
+            start.push_back({0, j});
+            start.push_back({n+1, j});
+        }
 
+        // Key 설정
         string key;
         cin >> key;
-        if(key[0] =='0') key = "";
+        bool keyArr[26] = {false};
+        if (key != "0") {
+            for (char c : key) {
+                keyArr[c - 'a'] = true;
+            }
+        }
+        
+        // BFS 
         int move[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        bool check[100][100] = {false};
-
         queue<pair<int, int>> q;
+        queue<pair<int, int>> door[26];
+        
         for (auto& s : start) {
-            q.push({s.first, s.second});
+            q.push(s);
             check[s.first][s.second] = true;
         }
 
@@ -45,43 +62,32 @@ int main() {
             int i = q.front().first;
             int j = q.front().second;
             q.pop();
-            char current = map[i][j];
-
-            if (current == '$') {
-                cnt++;
-                map[i][j] = '.'; 
-            }
-
-            if ('A' <= current && current <= 'Z') {
-                if (key.find(tolower(current)) != string::npos) { 
-                    map[i][j] = '.';  
-                } else {
-                    continue;  
-                }
-            }
-
-            if ('a' <= current && current <= 'z') {
-                if (key.find(current) == string::npos) {
-                    key += current;  
-                    for (int i = 0; i < n; i++) {
-                        for (int j = 0; j < m; j++) check[i][j] = false; 
-                    }
-                   
-                    while (!q.empty()) q.pop();
-                   
-                    for (auto& s : start) {
-                        q.push({s.first, s.second});
-                        check[s.first][s.second] = true;
-                    }
-                    continue;
-                }
-            }
 
             for (auto& mv : move) {
                 int mi = i + mv[0];
                 int mj = j + mv[1];
-                if (mi < 0 || mi >= n || mj < 0 || mj >= m) continue;
+                if (mi < 0 || mi > n+1 || mj < 0 || mj > m+1) continue;
                 if (map[mi][mj] != '*' && !check[mi][mj]) {
+                    char next = map[mi][mj];
+                    if (next == '$') {
+                        cnt++;
+                    }
+                    else if ('A' <= next && next <= 'Z') {
+                        if (!keyArr[next - 'A']) { 
+                            door[next - 'A'].push({mi, mj});
+                            continue;  
+                        }
+                    }
+                    else if ('a' <= next && next <= 'z') {
+                        if (!keyArr[next - 'a']) {
+                            keyArr[next - 'a'] = true;
+                            while (!door[next - 'a'].empty()) {
+                                q.push(door[next - 'a'].front());
+                                door[next - 'a'].pop();
+                            }
+                        }
+                    }
+
                     q.push({mi, mj});
                     check[mi][mj] = true;
                 }
