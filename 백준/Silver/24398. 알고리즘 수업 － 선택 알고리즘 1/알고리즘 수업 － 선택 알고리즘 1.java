286b32d295397dcb2b31fadc37a9ab9a4bb42e27
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-10-12 16:32:55 +0900

    [Silver V] Title: 알고리즘 수업 - 선택 알고리즘 1, Time: 232 ms, Memory: 17268 KB -BaekjoonHub

--- 백준/Silver/24398. 알고리즘 수업 － 선택 알고리즘 1/알고리즘 수업 － 선택 알고리즘 1.java
+++ 백준/Silver/24398. 알고리즘 수업 － 선택 알고리즘 1/알고리즘 수업 － 선택 알고리즘 1.java
@@ -1,60 +1,68 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
     static int K, k;
-    static boolean find = false;
 
-    static void select(int[] arr, int N, int Q) {
-        int l = 0, r = N - 1;
-        while (l <= r) {
-            int pivot = partition(arr, l, r);
-            if (pivot == Q - 1 || find) return;
-            if (Q - 1 < pivot) r = pivot - 1;
-            else l = pivot + 1;
+    static boolean find = false;
+    static void select(int[] arr, int rr, int q) {
+        int l = 0, r = rr;
+        while (l <= r) { //메모리 초과 방지하는 법 -> 재귀 대신 while 사용...
+            int tmp = partition(arr, l, r);
+            if (tmp == q - 1 ||find) return;
+            if (q - 1 < tmp) r = tmp - 1;
+            else l = tmp + 1;
         }
     }
 
     static int partition(int[] arr, int l, int r) {
         int pivot = arr[r];
-        int i = l - 1;
+        int i = l - 1; // pivot 보다 작은 값
         for (int j = l; j < r; j++) {
             if (arr[j] <= pivot) {
                 i++;
-                swap(arr, i, j);
+                k++;
+                if (K == k) {
+                    if (arr[i] < arr[j]) System.out.print(arr[i] + " " + arr[j]);
+                    else System.out.print(arr[j] + " " + arr[i]);
+                    System.out.println();
+                    find=true;
+                    return j;
+                }
+                int tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
             }
         }
-        if(i+1!=r) swap(arr, i + 1, r);
-        return i + 1;
-    }
-
-    static void swap(int[] arr, int i, int j) {
-        k++;
-        if (K == k) {
-            if (arr[i] < arr[j]) System.out.println(arr[i] + " " + arr[j]);
-            else System.out.println(arr[j] + " " + arr[i]);
-            find = true;
+        if (i + 1 != r) {
+            k++;
+            if (K == k) {
+                if (arr[i+1] < arr[r]) System.out.print(arr[i+1] + " " + arr[r]);
+                else System.out.print(arr[r] + " " + arr[i+1]);
+                System.out.println();
+                find=true;
+            }
+            int tmp = arr[i+1];
+            arr[i+1] = arr[r];
+            arr[r] = tmp;
         }
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        return i+1;
     }
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
         int Q = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        k = 0;
-
+        k=0;
         int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        select(arr, N, Q);
+        select(arr, N - 1, Q);
         if (!find) System.out.println(-1);
         br.close();
     }
