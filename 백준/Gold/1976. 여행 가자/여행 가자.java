76d3f807f2fc1395497cd720befe744d1d82d7ad
y2arttt <149859233+y2arttt@users.noreply.github.com>
2024-11-28 20:18:28 +0900

    [Gold IV] Title: 여행 가자, Time: 452 ms, Memory: 55648 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,47 +1,71 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static int[] p = new int[201];
 
-    static int find(int x) {
-        if (p[x] == x) return x;
-        return p[x] = find(p[x]);
+    public static int find(int[] p,int i){
+        if(i != p[i]){
+            p[i] = find(p,p[p[i]]);
+
+        }
+        return p[i];
+
     }
-
-    static void uni(int x, int y) {
-        x = find(x);
-        y = find(y);
-        if (x < y) p[y] = x;
-        else p[x] = y;
+    public static void union(int[] p,int i,int j){
+        int rootI = find(p,i);
+        int rootJ = find(p,j);
+        if(rootI == rootJ){
+            return;
+        }
+        else if(rootI < rootJ){
+            p[rootJ] = rootI;
+        }
+        else {
+            p[rootI] = p[rootJ];
+        }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+            Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= n; i++) p[i] = i;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int x = sc.nextInt();
-                if (x == 1) {
-                    uni(i, j);
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[][] arr = new int[n+1][n+1];
+            int[] p = new int[n+1];
+            int[] plan = new int[m];
+            for(int i = 1; i <= n; i++){
+                p[i] = i;
+            }
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    arr[i][j] = sc.nextInt();
                 }
             }
-        }
-
-        int root = -1;
-        for (int i = 0; i < m; i++) {
-            int x = sc.nextInt();
-            if (i == 0) root = find(x);
+            for(int i = 0; i < m; i++){
+                plan[i] = sc.nextInt();
+            }
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if(arr[i][j] == 1){
+                        union(p,i,j);
+                    }
+                }
+            }
+            int s = p[plan[0]];
+            boolean flag = false;
+            for(int pl : plan){
+                if(p[pl] != s){
+                    flag = true;
+                    break;
+                }
+            }
+/*            for(int pp : p){
+                System.out.print(pp+" ");
+            }*/
+            if(flag){
+                System.out.println("NO");
+            }
             else {
-                if (find(root) != find(x)) {
-                    System.out.println("NO");
-                    return;
-                }
+                System.out.println("YES");
             }
-        }
-        System.out.println("YES");
     }
 }
